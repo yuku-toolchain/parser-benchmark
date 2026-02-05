56cde7419ee74fd8e21c41ce9fd0dccb29214df3
arshad <arshadpyaseen@gmail.com>
2026-02-05 18:19:38 +0530

    update to latest zig master

--- zig/src/jam.zig
+++ zig/src/jam.zig
@@ -3,22 +3,18 @@ const jam = @import("jam");
 
 const Parser = jam.Parser;
 
-pub fn main() !void {
-    const allocator = std.heap.page_allocator;
+const cwd = std.Io.Dir.cwd();
 
-    const args = try std.process.argsAlloc(allocator);
-    defer std.process.argsFree(allocator, args);
+pub fn main(init: std.process.Init) !void {
+    const Io = init.io;
+    const allocator = init.arena.allocator();
+
+    const args = try init.minimal.args.toSlice(allocator);
 
     const path = args[1];
-    const contents = try std.fs.cwd().readFileAlloc(path, allocator, std.Io.Limit.limited(10 * 1024 * 1024));
-    defer allocator.free(contents);
+    const contents = try cwd.readFileAlloc(Io, path, allocator, std.Io.Limit.limited(10 * 1024 * 1024));
 
-    var arena = std.heap.ArenaAllocator.init(allocator);
-    defer arena.deinit();
-
-    const arena_allocator = arena.allocator();
-
-    var parser = try Parser.init(arena_allocator, contents, .{ .source_type = .module });
+    var parser = try Parser.init(allocator, contents, .{ .source_type = .module });
     defer parser.deinit();
 
     var result = try parser.parse();
--- zig/src/yuku.zig
+++ zig/src/yuku.zig
@@ -3,15 +3,16 @@ const yuku = @import("yuku");
 
 const js = yuku.js;
 
-pub fn main() !void {
-    const allocator = std.heap.page_allocator;
+const cwd = std.Io.Dir.cwd();
 
-    const args = try std.process.argsAlloc(allocator);
-    defer std.process.argsFree(allocator, args);
+pub fn main(init: std.process.Init) !void {
+    const Io = init.io;
+    const allocator = init.arena.allocator();
+
+    const args = try init.minimal.args.toSlice(allocator);
 
     const path = args[1];
-    const contents = try std.fs.cwd().readFileAlloc(path, allocator, std.Io.Limit.limited(10 * 1024 * 1024));
-    defer allocator.free(contents);
+    const contents = try cwd.readFileAlloc(Io, path, allocator, std.Io.Limit.limited(10 * 1024 * 1024));
 
     const tree = try js.parse(std.heap.page_allocator, contents, .{
         .lang = js.Lang.fromPath(path),
