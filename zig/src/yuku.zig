b5c6fdf13e17d220acb209bb9a611af673ae0e2a
arshad <arshadpyaseen@gmail.com>
2026-02-11 14:13:29 +0530

    update

--- zig/src/yuku.zig
+++ zig/src/yuku.zig
@@ -4,7 +4,7 @@ const yuku_parser = @import("yuku_parser");
 const source = @embedFile("source");
 
 pub fn main(_: std.process.Init) !void {
-    const tree = try yuku_parser.parse(std.heap.c_allocator, source, .{
+    const tree = try yuku_parser.parse(std.heap.page_allocator, source, .{
         .lang = yuku_parser.Lang.fromPath("bench.js"),
         .source_type = yuku_parser.SourceType.fromPath("bench.js"),
     });
